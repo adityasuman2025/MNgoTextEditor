6f4ce821f041920c0d8ba9c3064ea129f1053be6
adityasuman2025 <adityasuman2025@gmail.com>
2026-06-20 08:06:53 +0530

    resume link updated

--- src/main.tsx
+++ src/main.tsx
@@ -411,7 +411,7 @@ const FILES_CONTENT = {
     },
     "resume.html": {
         "title": "Resume",
-        "content": '<div style="text-align: center;"><a class="resumeBtn" target="_blank" href="https://drive.google.com/file/d/1FKkhU9SEod7knpwsJOJJ3Y1QnRZkpdEQ/view?usp=share_link">Download Resume</a></div>'
+        "content": '<div style="text-align: center;"><a class="resumeBtn" target="_blank" href="https://drive.google.com/file/d/1LqXZ4zlcoT_hm1QKgUxqbF_zzRHQEq68/view?usp=sharing">Download Resume</a></div>'
     },
     "follow_me.html": {
         "title": "Follow Me",
